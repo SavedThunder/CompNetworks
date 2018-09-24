a18f83e7664c81e40760aed6ce906a552abbd393
Unknown <krauskmr@mail.uc.edu>
2018-09-24 19:33:16 -0400

    I hope this is enough

---
+++ server_protocol/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainText = new System.Windows.Forms.TextBox();
+            this.StartCon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainText
+            // 
+            this.MainText.Location = new System.Drawing.Point(39, 36);
+            this.MainText.Multiline = true;
+            this.MainText.Name = "MainText";
+            this.MainText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MainText.Size = new System.Drawing.Size(316, 305);
+            this.MainText.TabIndex = 1;
+            this.MainText.TextChanged += new System.EventHandler(this.MainText_TextChanged);
+            // 
+            // StartCon
+            // 
+            this.StartCon.Location = new System.Drawing.Point(419, 95);
+            this.StartCon.Name = "StartCon";
+            this.StartCon.Size = new System.Drawing.Size(93, 54);
+            this.StartCon.TabIndex = 3;
+            this.StartCon.Text = "Start Connection";
+            this.StartCon.UseVisualStyleBackColor = true;
+            this.StartCon.Click += new System.EventHandler(this.StartCon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.StartCon);
+            this.Controls.Add(this.MainText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox MainText;
+        private System.Windows.Forms.Button StartCon;
+    }
+}
+
